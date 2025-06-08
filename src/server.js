8a09736c9808421e871af027ce6fc4b18d67927f
3 det <datb2404981@student.ctu.edu.vn>
2025-06-08 09:57:20 +0700

    app\  boostrar

--- src/server.js
+++ src/server.js
@@ -10,14 +10,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
+app.set(express.static(path.join(__dirname, 'public/img')));
+
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 app.set('views',path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
